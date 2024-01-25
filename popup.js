6a196689e7bb2d87bfdda9493125232eb610e648
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2024-01-25 20:57:16 +0530

    added-empty-editor-page

--- popup.js
+++ popup.js
@@ -8,13 +8,26 @@ function takeScreenshot() {
 	});
 }
 
+function openEditor() {
+	chrome.tabs.create({
+		url: chrome.runtime.getURL("editor.html")
+	});
+}
+
 // Attach the evenet listener to the button
 document.addEventListener("DOMContentLoaded", function() {
 	const screenshotBtn = document.getElementById("screenshotBtn");
+	const openEditorBtn = document.getElementById("openEditorBtn");
 	console.log("inside");
 
 	if(screenshotBtn) {
 		screenshotBtn.addEventListener("click", takeScreenshot);
+	} else {
+		console.error("Button not found");
+	}
+
+	if(openEditorBtn) {
+		openEditorBtn.addEventListener("click", openEditor);
 	} else {
 		console.error("Button not found");
 	}
