d02f635f23bb4e8692f30c19d4205d33e6f16f3f
Anuj Rajaram Jadhav <76788689+anuj1511@users.noreply.github.com>
2024-01-25 23:04:30 +0530

    editor: image loaded in editor

--- popup.js
+++ popup.js
@@ -1,36 +1,42 @@
 function takeScreenshot() {
-	console.log("func called");
-	chrome.tabs.captureVisibleTab({format: "png"}, function (screenshotUrl) {
-		const link = document.createElement("a");
-		link.href = screenshotUrl;
-		link.download = "screenshot.png";
-		link.click();
-	});
+    console.log("takeScreenshot called");
+
+    return new Promise((resolve, reject) => {
+        chrome.tabs.captureVisibleTab({format: "png"}, function (screenshotUrl) {
+            localStorage.setItem("screenshotUrl", screenshotUrl);
+            resolve(screenshotUrl);
+        });
+    });
 }
 
 function openEditor() {
-	chrome.tabs.create({
-		url: chrome.runtime.getURL("editor.html")
+    chrome.tabs.create({
+        url: chrome.runtime.getURL("editor.html")
+    });
+}
+
+function handleClick() {
+	console.log("handle click");
+	takeScreenshot().then(openEditor).catch(function(error) {
+		console.error("Error taking screenshot:", error);
 	});
 }
 
 // Attach the evenet listener to the button
 document.addEventListener("DOMContentLoaded", function() {
 	const screenshotBtn = document.getElementById("screenshotBtn");
-	const openEditorBtn = document.getElementById("openEditorBtn");
+	// const openEditorBtn = document.getElementById("openEditorBtn");
 	console.log("inside");
 
-	if(screenshotBtn) {
-		screenshotBtn.addEventListener("click", takeScreenshot);
+	if(screenshotBtn) {		
+		screenshotBtn.addEventListener("click", handleClick);
 	} else {
 		console.error("Button not found");
 	}
 
-	if(openEditorBtn) {
-		openEditorBtn.addEventListener("click", openEditor);
-	} else {
-		console.error("Button not found");
-	}
+	// if(openEditorBtn) {
+	// 	openEditorBtn.addEventListener("click", openEditor);
+	// } else {
+	// 	console.error("Button not found");
+	// }
 })
-
-console.log("hello");
